9a9d9c41a7e9bd64c128b995fa8e02b59500cb3f
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-10-21 16:44:14 -0500

    ajuete error

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import routerProduct from "./src/routes/product.Routes.js";
 import connectDB from "./src/Config/Db.js";
 config()
 const PORT = process.env.PORT || 3000;
-connectDB()
+
 const app = express();
 app.use(cors({
     origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PATCH', 'PUT' ,'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
+connectDB()
 app.options('*', cors());
 
 
